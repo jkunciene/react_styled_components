d0c620660eccaea4507447c3ef7f7ecb42b29a43
jkunciene <j.kunciene@gmail.com>
2021-03-24 15:45:53 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useInterval } from 'react-use';
+import styled from 'styled-components'
 
+const Rain = () => {
+      const [emojisToRender, setEmojiToRender] = useState([{key: 0 , emoji: '☠️', offset: 0 }])
+
+      useInterval(() => {
+        if (emojisToRender.length > 10 ) {
+            emojisToRender.shift();
+        }
+        const offset = Math.floor(Math.random() * 1000);
+        const key = offset + Math.floor(Math.random() * 100000);
+        const emoji = '☠️';
+
+        emojisToRender.push({offset, key, emoji})
+          setEmojiToRender([...emojisToRender])
+      }, 500);
+
+        return(
+           <SuperContainer>
+              {emojisToRender.map(({key, emoji, offset}) => {
+               return (
+                  <EmojiContainer key={key} offset={offset}>
+                    {emoji}
+                  </EmojiContainer>  
+
+               )
+             })}   
+          </SuperContainer>
+        )
+               
+       
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (   
+     <>
+        <Rain/>        
+      </>
+    
   );
 }
 
 export default App;
+
+const SuperContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+const EmojiContainer = styled.div`
+  @keyframes falldown {
+    0% { margin-top: 0; }
+    100% { margin-top: 400px; }
+    }
+
+    margin-left: ${props => props.offset}px;
+
+    font-size: 48px;
+    animation-name: falldown;
+    animation-duration: 4s;
+`;
